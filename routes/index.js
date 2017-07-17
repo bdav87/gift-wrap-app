0858cfcefb3ce09199c12ba87024fbc21663e693
bdav87 <bdav87@gmail.com>
2017-07-17 14:54:51 -0500

    trying auth again

--- routes/index.js
+++ routes/index.js
@@ -22,7 +22,7 @@ router.get('/', function(req, res, next) {
 /* GET auth page. */
 router.get('/auth', function(req, res) {
   bigCommerce.authorise(req.query, function(err, data){
-    res.send('index', {data: data});
+    res.send('index', {stuff: data});
   })
 });
 
