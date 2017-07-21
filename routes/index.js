6c94774b4d8aef445c05356d8ede84f596a41832
bdav87 <bdav87@gmail.com>
2017-07-21 02:47:13 -0500

    check for hooks

--- routes/index.js
+++ routes/index.js
@@ -59,7 +59,7 @@ router.get('/uninstall', function(req, res, next) {
 });
 
 router.post('/status', function(req, res) {
-
+  checkWebHooks();
   console.log('req: ' + req.body.status);
 
   res.send('status updated to ' + req.body.status);
