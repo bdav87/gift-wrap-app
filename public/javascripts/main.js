a148a63fa3eac624bb4faeadedb2f250a191475a
bdav87 <bdav87@gmail.com>
2017-07-21 04:12:03 -0500

    auth fix18

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,7 +9,7 @@ $('#status-list').change(function(){
       "content-type": "json"
     },
     success: function(data){
-      $('#responseCheck').text('ok' + res);
+      $('#responseCheck').text('ok' + JSON.stringify(res));
     },
     error: function(){console.log('whyyyyyyy');}
   })
